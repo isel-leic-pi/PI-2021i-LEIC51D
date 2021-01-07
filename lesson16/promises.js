e518a2cf84ae02a16b2edb4856a3d0a21c1b0926
Luís Falcão <lfalcao@cc.isel.pt>
2021-01-07 13:34:45 +0000

    Lesson 16: Primises

--- lesson16/promises.js
+++ lesson16/promises.js
@@ -1,30 +1,73 @@
-function mulWithPromisesSyncImplementation(a,b) {
-  return Promise.resolve(a*b)
-}
 
-function mulWithPromisesAsyncImplementation(a, b) {
-  return new Promise(promiseExecutor)
+async function divWithDualAsyncImplementation(a, b, cb) {
+  let p = new Promise(promiseExecutor)
+  if(!cb) {
+    // Return a promise
+    return p
+  } else {
+    // call the callback when the promise is completed 
+    p.then(res => cb(null, res)).catch(err => cb(err))
+  }
+  
   
   function promiseExecutor(successFunc, rejectFunc) {
+    if(b == 0) {
+      return rejectFunc("0 is an invalida value for divisor")
+    }
     setTimeout(() => {
-      successFunc(a*b)
+      successFunc(a/b)
     }, 5000) 
   }
 }
 
-console.log("Now with promises")
 
-mulWithPromisesSyncImplementation(9,9)
-  .then(res => console.log(res))
-  .catch(err => console.log(err))
+// Usage with callback
+divWithDualAsyncImplementation(9,9, processResult)
+divWithDualAsyncImplementation(9,0, processResult)
 
-console.log("after calling mulWithPromisesSyncImplementation")
+// Usage with Promise
+divWithDualAsyncImplementation(20,10)
+  .then(processSuccess)
+  .catch(processError)
+  .finally(() => console.log("finally with success"))
+divWithDualAsyncImplementation(20,0)
+  .then(processSuccess)
+  .catch(processError)
+  .finally(() => console.log("finally with error"))
+  ;
 
-mulWithPromisesAsyncImplementation(7, 8)
-  .then(res => console.log(res))
-  .catch(err => console.log(err))
-  console.log("after calling mulWithPromisesSyncImplementation")
+// Usage with async/await
+(async function () {
+  try {
+    // Version 1: Without Promise.all each asynchronous operations starts when the previous ends 
+    // let res1 = await divWithDualAsyncImplementation(30,10)
+    // let res2 = await divWithDualAsyncImplementation(40,10)
+    // let res3 = await divWithDualAsyncImplementation(50,10)
+    // let res = [res1, res2, res3]
 
-console.log("main code ended")
+    // Version 2: With Promise.all each asynchronous operations starts "at the same time"
+    let res1 = divWithDualAsyncImplementation(30,10)
+    let res2 = divWithDualAsyncImplementation(40,10)
+    let res3 = divWithDualAsyncImplementation(50,10)
+    let res = await Promise.all([res1, res2, res3])
+    
+    processSuccess(res)
+  } catch(err) {
+    processError(err)
+  } 
+})()
+
+
+function processResult(err, res) {
+  err ? processError(err) : processSuccess(res)
+}
+
+function processSuccess(res) {
+  console.log("Success:", res)
+}
+
+function processError(err) {
+  console.log("Error:", err)
+}
 
 
